cda5d9f08ec45604953b4ce9ff737a993100bd09
David Barina <ibarina@fit.vutbr.cz>
2020-03-22 12:10:00 +0100

    2.588300

--- x3.c
+++ x3.c
@@ -176,7 +176,7 @@ size_t calc_cost(struct elem *e, char *curr_pos)
 	assert(freq > 0);
 
 	/* cost function to be optimized */
-	size_t cost = /*16*/8 * freq;
+	size_t cost = /*8*/4 * freq;
 
 	if (dist < BACKWARD_WINDOW + MAX_MATCH_LEN + 1) {
 		cost += 1 * (BACKWARD_WINDOW + MAX_MATCH_LEN + 1 - dist);
