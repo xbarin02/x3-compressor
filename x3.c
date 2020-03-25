38586160b12780ee32b383738a13656a23c30cb5
David Barina <ibarina@fit.vutbr.cz>
2020-03-25 13:37:43 +0100

    update

--- x3.c
+++ x3.c
@@ -15,7 +15,7 @@
 #define MAX_MATCH_LEN (1 << MATCH_LOGSIZE)
 
 /* found empirically */
-#define TCOUNT /*10*/11
+#define TCOUNT 11
 
 /* recompute Golomb-Rice codes after... */
 #define RESET_INTERVAL 256
