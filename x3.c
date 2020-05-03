7f9ac6edad044e0c1f78c9013f5dfdef1c8b4bfc
David Barina <ibarina@fit.vutbr.cz>
2020-05-03 16:57:38 +0200

    decoding works!

--- x3.c
+++ x3.c
@@ -168,6 +168,21 @@ void ctx_encode_tag_without_update(struct bio *bio, struct ctx *ctx, size_t tag)
 	}
 }
 
+size_t ctx_decode_tag_without_update(struct bio *bio, struct ctx *ctx)
+{
+	size_t item_index = 0;
+
+	if (ctx->items > 1) {
+		/* decode item_index */
+		gr_recalc_k(&ctx->gr);
+		item_index = (size_t)bio_read_gr(bio, ctx->gr.opt_k);
+	} else {
+		/* there is only one */
+	}
+
+	return ctx->arr[item_index].tag;
+}
+
 void ctx0_realloc()
 {
 	ctx0 = realloc(ctx0, tag_pair_get_size() * sizeof(struct ctx));
@@ -185,7 +200,8 @@ void ctx0_realloc()
 #define SIZEOF_BITCODE_CTX2 4 /* 0001 */
 #define SIZEOF_BITCODE_IDX2 5 /* 00001*/
 #define SIZEOF_BITCODE_CTX3 6 /* 000001 */
-#define SIZEOF_BITCODE_NEW  /*6*/7 /* 000000 */
+#define SIZEOF_BITCODE_NEW  7 /*6*/ /* 000000 */
+#define SIZEOF_BITCODE_EOF  8 /* end of input */
 
 /* list of events */
 enum {
@@ -201,6 +217,139 @@ enum {
 size_t events[7];
 
 size_t sizes[7];
+
+/* return index */
+size_t decode_tag(size_t decision, struct bio *bio, size_t prev_context1, size_t context1, size_t context2, size_t pindex)
+{
+	// order of tags is (prev_context1, context1, tag)
+	struct tag_pair ctx_pair = make_tag_pair(prev_context1, context1); /* previous two tags */
+
+	size_t ctx0_id = tag_pair_query(&ctx_pair); /* convert (prev_context1, context1) to linear id */
+	if (ctx0_id == (size_t)-1) {
+		// not found context id, default to 0
+		ctx0_id = 0;
+	}
+
+	struct ctx *c0 = ctx0 + ctx0_id;
+	struct ctx *c1 = ctx1 + context1;
+	struct ctx *c2 = ctx2 + context2;
+	struct ctx *c3 = ctx3 + (context2 & 255);
+
+	int mode; /* for stats */
+	size_t tag;
+	size_t index;
+	switch (decision) {
+		case SIZEOF_BITCODE_CTX0:
+			tag = ctx_decode_tag_without_update(bio, c0);
+			index = dict_get_index_by_tag(tag);
+			mode = E_CTX0;
+			break;
+		case SIZEOF_BITCODE_CTX1:
+			tag = ctx_decode_tag_without_update(bio, c1);
+			index = dict_get_index_by_tag(tag);
+			mode = E_CTX1;
+			break;
+		case SIZEOF_BITCODE_CTX2:
+			tag = ctx_decode_tag_without_update(bio, c2);
+			index = dict_get_index_by_tag(tag);
+			mode = E_CTX2;
+			break;
+		case SIZEOF_BITCODE_CTX3:
+			tag = ctx_decode_tag_without_update(bio, c3);
+			index = dict_get_index_by_tag(tag);
+			mode = E_CTX3;
+			break;
+		case SIZEOF_BITCODE_IDX1:
+			// bio_write_gr(bio, gr_idx1.opt_k, (uint32_t)index);
+			index = (size_t)bio_read_gr(bio, gr_idx1.opt_k);
+			tag = dict_get_tag_by_index(index);
+			mode = E_IDX1;
+			break;
+		case SIZEOF_BITCODE_IDX2:
+			// bio_write_gr(bio, gr_idx2.opt_k, (uint32_t)(index - pindex));
+			index = (size_t)bio_read_gr(bio, gr_idx2.opt_k) + pindex;
+			tag = dict_get_tag_by_index(index);
+			mode = E_IDX2;
+			break;
+		default:
+			abort();
+	}
+
+	events[mode]++;
+// 	sizes[mode] += size;
+
+	// update Golomb-Rice models
+
+	// mode = E_CTX0
+	if (ctx_query_tag_item(c0, tag) != NULL) {
+		ctx_encode_tag(c0, tag);
+	}
+	// mode = E_CTX1
+	if (ctx_query_tag_item(c1, tag) != NULL) {
+		ctx_encode_tag(c1, tag);
+	}
+	// mode = E_CTX2
+	if (ctx_query_tag_item(c2, tag) != NULL) {
+		ctx_encode_tag(c2, tag);
+	}
+	// mode = E_CTX3
+	if (ctx_query_tag_item(c3, tag) != NULL) {
+		ctx_encode_tag(c3, tag);
+	}
+	// mode = E_IDX1
+	if (mode == E_IDX1) {
+		gr_update_model(&gr_idx1, index);
+	}
+	// mode = E_IDX2
+	if (mode == E_IDX2) {
+		gr_update_model(&gr_idx2, index - pindex);
+	}
+
+	// update contexts
+
+	if (ctx_query_tag_item(c0, tag) == NULL) {
+		ctx_add_tag(c0, tag);
+	} else {
+		ctx_item_inc_freq(c0, tag);
+	}
+	ctx_sort(c0);
+
+	if (ctx_query_tag_item(c1, tag) == NULL) {
+		ctx_add_tag(c1, tag);
+	} else {
+		ctx_item_inc_freq(c1, tag);
+	}
+	ctx_sort(c1);
+
+	if (ctx_query_tag_item(c2, tag) == NULL) {
+		ctx_add_tag(c2, tag);
+	} else {
+		ctx_item_inc_freq(c2, tag);
+	}
+	ctx_sort(c2);
+
+	if (ctx_query_tag_item(c3, tag) == NULL) {
+		ctx_add_tag(c3, tag);
+	} else {
+		ctx_item_inc_freq(c3, tag);
+	}
+	ctx_sort(c3);
+
+	/* (context1, tag) constitutes new pair of tags */
+
+	struct tag_pair pair = make_tag_pair(context1, tag);
+
+	if (tag_pair_query(&pair) == (size_t)-1) {
+		// add new context
+		if (!tag_pair_can_add()) {
+			tag_pair_enlarge();
+			ctx0_realloc();
+		}
+		tag_pair_add(&pair);
+	}
+
+	return index;
+}
 
 /* encode dict[index].tag in context, rather than index */
 void encode_tag(struct bio *bio, size_t prev_context1, size_t context1, size_t context2, size_t index, size_t pindex)
@@ -392,6 +541,101 @@ void encode_match(struct bio *bio, char *p, size_t len)
 	}
 }
 
+void decode_match(struct bio *bio, char *p, size_t *p_len)
+{
+	/* SIZEOF_BITCODE_NEW has been already read */
+
+	*p_len = (size_t)(bio_read_bits(bio, MATCH_LOGSIZE) + 1);
+
+	for (size_t c = 0; c < *p_len; ++c) {
+		*(p + c) = (char)bio_read_bits(bio, 8);
+	}
+}
+
+char *decompress(char *ptr, struct bio *bio)
+{
+	size_t prev_context1 = 0; /* previous context1 */
+	size_t context1 = 0; /* last tag */
+	size_t context2 = 0; /* last two bytes */
+	size_t pindex = (size_t)-1; /* previous index */
+
+	char *p = ptr;
+
+	for (;;) {
+		size_t decision = (size_t)(bio_read_gr(bio, 0) + 1);
+
+		if (decision == SIZEOF_BITCODE_EOF) {
+			break;
+		} else if (decision == SIZEOF_BITCODE_NEW) {
+			/* new match */
+
+			size_t len;
+
+			decode_match(bio, p, &len);
+
+			struct elem e;
+			elem_fill(&e, p, len);
+
+			assert(dict_query_elem(&e) == 0);
+
+			if (!dict_can_insert_elem()) {
+				dict_enlarge();
+				enlarge_ctx1();
+			}
+
+			dict_insert_elem(&e);
+
+			p += len;
+
+			prev_context1 = 0;
+			context1 = 0;
+
+			if (p >= ptr + 2) {
+				context2 = make_context2(p - 2);
+			}
+
+			dict_update_costs(p);
+
+			pindex = (size_t)-1;
+
+			events[E_NEW]++;
+
+			sizes[E_NEW] += SIZEOF_BITCODE_NEW + MATCH_LOGSIZE + 8 * len;
+			stream_size_raw_str += 8 * len;
+		} else {
+			/* in dictionary */
+
+			size_t index = decode_tag(decision, bio, prev_context1, context1, context2, pindex);
+
+			size_t len = dict_get_len_by_index(index);
+
+			prev_context1 = context1;
+			context1 = dict_get_tag_by_index(index);
+
+			dict_set_last_pos(index, p);
+
+			/* FIXME: put uncompressed fragment */
+			extern struct elem *dict;
+			for (size_t c = 0; c < len; ++c) {
+				p[c] = dict[index].s[c];
+			}
+
+			p += len;
+
+			if (p >= ptr + 2) {
+				context2 = make_context2(p - 2);
+			}
+
+			/* recalc all costs, sort */
+			dict_update_costs(p);
+
+			pindex = index;
+		}
+	}
+
+	return p;
+}
+
 void compress(char *ptr, size_t size, struct bio *bio)
 {
 	char *end = ptr + size;
@@ -476,6 +720,9 @@ void compress(char *ptr, size_t size, struct bio *bio)
 			stream_size_raw_str += 8 * len;
 		}
 	}
+
+	/* signal end of input */
+	bio_write_gr(bio, 0, SIZEOF_BITCODE_EOF - 1);
 }
 
 void destroy()
@@ -505,9 +752,23 @@ void destroy()
 	tag_pair_destroy();
 }
 
+enum {
+	COMPRESS,
+	DECOMPRESS
+};
+
+
 int main(int argc, char *argv[])
 {
-	parse: switch (getopt(argc, argv, "ht:w:T:")) {
+	int mode = COMPRESS;
+
+	parse: switch (getopt(argc, argv, "zdht:w:T:")) {
+		case 'z':
+			mode = COMPRESS;
+			goto parse;
+		case 'd':
+			mode = DECOMPRESS;
+			goto parse;
 		case 'h':
 			// print_help(argv[0]);
 			return 0;
@@ -526,118 +787,161 @@ int main(int argc, char *argv[])
 		;
 	}
 
-	char *path;
+	FILE *istream = NULL, *ostream = NULL;
 
 	switch (argc - optind) {
 		case 0:
-			path = "enwik6";
+			istream = fopen("enwik8", "r");
+			ostream = fopen("output.bit", "w");
 			break;
 		case 1:
-			path = argv[optind];
+			istream = fopen(argv[optind], "r");
+			ostream = fopen("output.bit", "w");
+			break;
+		case 2:
+			istream = fopen(argv[optind + 0], "r");
+			ostream = fopen(argv[optind + 1], "w");
 			break;
 		default:
 			fprintf(stderr, "Unexpected argument\n");
 			abort();
 	}
 
-	printf("max match count: %i\n", get_max_match_count());
-	printf("forward window: %zu\n", get_forward_window());
-	printf("threads: %i\n", get_num_threads());
+	fprintf(stderr, "%s\n", mode == COMPRESS ? "Compressing..." : "Decompressing...");
 
-	printf("path: %s\n", path);
-
-	FILE *stream = fopen(path, "r");
-
-	if (stream == NULL) {
+	if (istream == NULL) {
+		fprintf(stderr, "Cannot open input file\n");
 		abort();
 	}
 
-	size_t size = fsize(stream);
-
-	char *ptr = malloc(size + get_forward_window());
-
-	if (ptr == NULL) {
+	if (ostream == NULL) {
+		fprintf(stderr, "Cannot open output file\n");
 		abort();
 	}
 
-	memset(ptr + size, 0, get_forward_window());
+	if (mode == COMPRESS) {
+		printf("max match count: %i\n", get_max_match_count());
+		printf("forward window: %zu\n", get_forward_window());
+		printf("threads: %i\n", get_num_threads());
 
-	fload(ptr, size, stream);
+		size_t size = fsize(istream);
 
-	fclose(stream);
+		char *iptr = malloc(size + get_forward_window());
 
-	create();
+		if (iptr == NULL) {
+			abort();
+		}
 
-	struct bio bio;
+		memset(iptr + size, 0, get_forward_window());
 
-	char *optr = malloc(size);
+		fload(iptr, size, istream);
 
-	if (optr == NULL) {
-		abort();
-	}
+		fclose(istream);
 
-	bio_open(&bio, optr, optr + size, BIO_MODE_WRITE);
+		create();
 
-	long start = wall_clock();
+		struct bio bio;
 
-	compress(ptr, size, &bio);
+		char *optr = malloc(size);
 
-	printf("elapsed time: %f\n", (wall_clock() - start) / (float)1000000000L);
+		if (optr == NULL) {
+			abort();
+		}
 
-	bio_close(&bio, BIO_MODE_WRITE);
+		bio_open(&bio, optr, optr + size, BIO_MODE_WRITE);
 
-	FILE *ostream = fopen("output.bit", "w");
+		long start = wall_clock();
 
-	if (NULL == ostream) {
-		abort();
-	}
+		compress(iptr, size, &bio);
 
-	char *end = (char *)bio.ptr;
+		printf("elapsed time: %f\n", (wall_clock() - start) / (float)1000000000L);
 
-	fsave(optr, end - optr, ostream);
+		bio_close(&bio, BIO_MODE_WRITE);
 
-	fclose(ostream);
+		char *end = (char *)bio.ptr;
 
-	free(optr);
+		fsave(optr, end - optr, ostream);
 
-	destroy();
+		fclose(ostream);
 
-	free(ptr);
+		free(optr);
 
-	size_t dict_hit_count = events[E_CTX0] + events[E_CTX1] + events[E_CTX2] + events[E_CTX3] + events[E_IDX1] + events[E_IDX2];
+		destroy();
 
-	size_t stream_size_gr = sizes[E_CTX0] + sizes[E_CTX1] + sizes[E_CTX2] + sizes[E_CTX3] + sizes[E_IDX1] + sizes[E_IDX2];
-	size_t stream_size = sizes[E_CTX0] + sizes[E_CTX1] + sizes[E_CTX2] + sizes[E_CTX3] + sizes[E_IDX1] + sizes[E_IDX2] + sizes[E_NEW];
+		free(iptr);
 
-	printf("input stream size: %zu\n", size);
-	printf("output stream size: %zu\n", (stream_size + 7) / 8);
-	printf("dictionary: hit %zu, miss %zu\n", dict_hit_count, events[E_NEW]);
+		size_t dict_hit_count = events[E_CTX0] + events[E_CTX1] + events[E_CTX2] + events[E_CTX3] + events[E_IDX1] + events[E_IDX2];
 
-	printf("codestream size: dictionary %zu / %f%% (Golomb-Rice), new %zu / %f%% (of which text %zu / %f%%)\n",
-		(stream_size_gr + 7) / 8, 100.f * stream_size_gr / stream_size,
-		(sizes[E_NEW] + 7) / 8, 100.f * sizes[E_NEW] / stream_size,
-		(stream_size_raw_str + 7) / 8, 100.f * stream_size_raw_str / stream_size
-	);
+		size_t stream_size_gr = sizes[E_CTX0] + sizes[E_CTX1] + sizes[E_CTX2] + sizes[E_CTX3] + sizes[E_IDX1] + sizes[E_IDX2];
+		size_t stream_size = sizes[E_CTX0] + sizes[E_CTX1] + sizes[E_CTX2] + sizes[E_CTX3] + sizes[E_IDX1] + sizes[E_IDX2] + sizes[E_NEW];
+
+		printf("input stream size: %zu\n", size);
+		printf("output stream size: %zu\n", (stream_size + 7) / 8);
+		printf("dictionary: hit %zu, miss %zu\n", dict_hit_count, events[E_NEW]);
+
+		printf("codestream size: dictionary %zu / %f%% (Golomb-Rice), new %zu / %f%% (of which text %zu / %f%%)\n",
+			(stream_size_gr + 7) / 8, 100.f * stream_size_gr / stream_size,
+			(sizes[E_NEW] + 7) / 8, 100.f * sizes[E_NEW] / stream_size,
+			(stream_size_raw_str + 7) / 8, 100.f * stream_size_raw_str / stream_size
+		);
 
 #if 1
-	printf("\x1b[37;1mcompression ratio: %f\x1b[0m\n", size / (float)((stream_size_gr + sizes[E_NEW] + 7) / 8));
+		printf("\x1b[37;1mcompression ratio: %f\x1b[0m\n", size / (float)((stream_size_gr + sizes[E_NEW] + 7) / 8));
 #else
-	printf("compression ratio: %f\n", size / (float)((stream_size_gr + sizes[E_NEW] + 7) / 8));
+		printf("compression ratio: %f\n", size / (float)((stream_size_gr + sizes[E_NEW] + 7) / 8));
 #endif
 
-	printf("number of events: ctx0 %zu, ctx1 %zu, ctx2 %zu, ctx3 %zu, miss1 %zu, miss2 %zu, new %zu\n",
-		events[E_CTX0], events[E_CTX1], events[E_CTX2], events[E_CTX3], events[E_IDX1], events[E_IDX2], events[E_NEW]);
-	printf("contexts size: ctx0 %f%%, ctx1 %f%%, ctx2 %f%%, ctx3 %f%%, miss1 %f%%, miss2 %f%%, new %f%%\n",
-		100.f * sizes[E_CTX0] / stream_size,
-		100.f * sizes[E_CTX1] / stream_size,
-		100.f * sizes[E_CTX2] / stream_size,
-		100.f * sizes[E_CTX3] / stream_size,
-		100.f * sizes[E_IDX1] / stream_size,
-		100.f * sizes[E_IDX2] / stream_size,
-		100.f * sizes[E_NEW] / stream_size
-	);
+		printf("number of events: ctx0 %zu, ctx1 %zu, ctx2 %zu, ctx3 %zu, miss1 %zu, miss2 %zu, new %zu\n",
+			events[E_CTX0], events[E_CTX1], events[E_CTX2], events[E_CTX3], events[E_IDX1], events[E_IDX2], events[E_NEW]);
+		printf("contexts size: ctx0 %f%%, ctx1 %f%%, ctx2 %f%%, ctx3 %f%%, miss1 %f%%, miss2 %f%%, new %f%%\n",
+			100.f * sizes[E_CTX0] / stream_size,
+			100.f * sizes[E_CTX1] / stream_size,
+			100.f * sizes[E_CTX2] / stream_size,
+			100.f * sizes[E_CTX3] / stream_size,
+			100.f * sizes[E_IDX1] / stream_size,
+			100.f * sizes[E_IDX2] / stream_size,
+			100.f * sizes[E_NEW] / stream_size
+		);
 
-	printf("context entries: ctx0 %zu, ctx1 %zu, ctx2 %zu, ctx3 %zu\n", tag_pair_get_elems(), dict_get_elems(), (size_t)65536, (size_t)256);
+		printf("context entries: ctx0 %zu, ctx1 %zu, ctx2 %zu, ctx3 %zu\n", tag_pair_get_elems(), dict_get_elems(), (size_t)65536, (size_t)256);
+	} else {
+		size_t isize = fsize(istream);
+		size_t osize = 64 * isize; /* at most 64 : 1 ratio */
+
+		char *iptr = malloc(isize);
+		char *optr = malloc(osize);
+
+		if (iptr == NULL) {
+			abort();
+		}
+
+		if (optr == NULL) {
+			abort();
+		}
+
+		fload(iptr, isize, istream);
+
+		fclose(istream);
+
+		create();
+
+		unsigned char *iend = (unsigned char *)iptr + isize;
+
+		struct bio bio;
+
+		bio_open(&bio, iptr, iend, BIO_MODE_READ);
+
+		char *oend = decompress((char *)optr, &bio);
+
+		bio_close(&bio, BIO_MODE_READ);
+
+		fsave(optr, oend - optr, ostream);
+
+		fclose(ostream);
+
+		free(iptr);
+		free(optr);
+	}
 
 	return 0;
 }
